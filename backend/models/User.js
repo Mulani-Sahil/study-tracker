439e250590f496119f91958601dfc52c8abec428
Mulani Sahil <161868836+Mulani-Sahil@users.noreply.github.com>
2026-02-15 14:48:56 +0530

    Uncomment User model and password methods

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,3 +1,106 @@
+// const mongoose = require('mongoose');
+// const bcrypt = require('bcryptjs');
+
+// const UserSchema = new mongoose.Schema({
+//   username: {
+//     type: String,
+//     required: [true, 'Please provide a username'],
+//     unique: true,
+//     trim: true,
+//     minlength: [3, 'Username must be at least 3 characters'],
+//     maxlength: [30, 'Username cannot exceed 30 characters']
+//   },
+//   email: {
+//     type: String,
+//     required: [true, 'Please provide an email'],
+//     unique: true,
+//     lowercase: true,
+//     trim: true,
+//     match: [
+//       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+//       'Please provide a valid email'
+//     ]
+//   },
+//   password: {
+//     type: String,
+//     required: [true, 'Please provide a password'],
+//     minlength: [6, 'Password must be at least 6 characters'],
+//     select: false // Don't return password by default
+//   },
+//   studyStreak: {
+//     type: Number,
+//     default: 0
+//   },
+//   lastStudyDate: {
+//     type: Date,
+//     default: null
+//   },
+//   lastLogin: {
+//     type: Date,
+//     default: Date.now
+//   },
+//   createdAt: {
+//     type: Date,
+//     default: Date.now
+//   }
+// });
+
+// // Hash password before saving
+// UserSchema.pre('save', async function(next) {
+//   if (!this.isModified('password')) {
+//     return next();
+//   }
+  
+//   try {
+//     const salt = await bcrypt.genSalt(10);
+//     this.password = await bcrypt.hash(this.password, salt);
+//     next();
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+// // Method to compare passwords
+// UserSchema.methods.comparePassword = async function(candidatePassword) {
+//   try {
+//     return await bcrypt.compare(candidatePassword, this.password);
+//   } catch (error) {
+//     throw new Error('Password comparison failed');
+//   }
+// };
+
+// // Method to update study streak
+// UserSchema.methods.updateStreak = function() {
+//   const today = new Date();
+//   today.setHours(0, 0, 0, 0);
+  
+//   if (!this.lastStudyDate) {
+//     // First study session
+//     this.studyStreak = 1;
+//     this.lastStudyDate = today;
+//   } else {
+//     const lastStudy = new Date(this.lastStudyDate);
+//     lastStudy.setHours(0, 0, 0, 0);
+    
+//     const diffTime = today - lastStudy;
+//     const diffDays = diffTime / (1000 * 60 * 60 * 24);
+    
+//     if (diffDays === 1) {
+//       // Consecutive day
+//       this.studyStreak += 1;
+//       this.lastStudyDate = today;
+//     } else if (diffDays === 0) {
+//       // Same day, no change
+//       return;
+//     } else {
+//       // Streak broken
+//       this.studyStreak = 1;
+//       this.lastStudyDate = today;
+//     }
+//   }
+// };
+
+// module.exports = mongoose.model('User', UserSchema);
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
